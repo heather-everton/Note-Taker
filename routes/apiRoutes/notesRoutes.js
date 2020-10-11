4af1b01c85f385588ac583fa8537f93ef65bfd49
Heather Everton <heather.everton@planplusonline.com>
2020-10-11 16:24:21 -0600

    fix post note route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
-let {
+const {
     createNewNote,
   } = require("../../lib/notes.js");
   
-const notes = require('../../db/db');
+const { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
-    createNewNote(req.body, notes)
-        .then( note => res.json(note));
+    console.log(notes)
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
-
+  
 module.exports  = router;
 
 
