81cb47d40eadb8b24876d767acf74d5f969b9235
Heather Everton <heather.everton@planplusonline.com>
2020-10-11 16:44:01 -0600

    update db file in server.js file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-const { notes } = require('./data/notes');
+const { notes } = require('./db/db');
 const PORT = process.env.PORT || 3001
 const app = express();
 app.use(express.urlencoded({ extended: true }));
